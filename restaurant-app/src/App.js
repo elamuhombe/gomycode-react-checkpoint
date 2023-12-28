5c559d406f064158d084cd4eada8cab0208d0fd6
Elaine Muhombe <emuhombe@gmail.com>
2023-12-29 00:36:09 +0300

    chore: update App.js

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Navbar, Nav, Button, Card } from "react-bootstrap";
+import { Container, Navbar, Nav, Button, Card, Forms } from "react-bootstrap";
 
 // React component
 
@@ -91,6 +91,45 @@ const MyComponent = () => {
             </div>
           </div>
         </div>
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+
+          {/* User login form */}
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" for="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </>
   );
